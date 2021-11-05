3a88e2663b035e29610094e8fdfe0aa408297cb6
BatoBalint <balint.bato@gmail.com>
2021-11-05 22:41:39 +0100

    kesz a form, asszem

--- newSauce.php
+++ newSauce.php
@@ -1,4 +1,36 @@
-<!DOCTYPE html>
+<?php
+  $methodGet = true;
+  $nameError = true;
+  $typeError = true;
+  $hotlvlError = true;
+
+  if ($_SERVER['REQUEST_METHOD'] === "POST") {
+    $methodGet = false;
+
+    $name = $_POST['name'] ?? '';
+    if ($name !== '') {
+      $nameError = false;
+    }
+
+    $type = $_POST['type'] ?? '';
+    if ($type !== '') {
+      $typeError = false;
+    }
+
+    $hotlvl = $_POST['hotlvl'] ?? '';
+    if ($hotlvl !== '') {
+      $hotlvlError = false;
+    }
+
+    $instorage = $_POST['instorage'] ?? 0;
+    $refilldate = $_POST['refilldate'] ?? new DateTime();
+
+    if (!$nameError && !$typeError && !$hotlvlError) {
+      $newSauce = new Sauce($name, $instorage, $refilldate, $type, $hotlvl);
+    }
+  }
+
+ ?><!DOCTYPE html>
 <html lang="hu" dir="ltr">
   <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -24,24 +56,24 @@
         <div class="col-sm-8 mx-auto">
           <form method="post">
             <div class="mb-3">
-              <label class="form-label">Szósz neve</label>
-              <input type="text" class="form-control">
+              <label class="form-label">Szósz neve <?php if (!$methodGet && $nameError) { echo '<span class="errorMessage">Nem lehet üres a név mező</span>'; } ?></label>
+              <input type="text" class="form-control" name="name" value="<?php echo $name; ?>">
             </div>
             <div class="mb-3">
               <label class="form-label">Mennyiség</label>
-              <input type="number" class="form-control">
+              <input type="number" class="form-control" name="instorage" value="<?php echo $instorage; ?>">
             </div>
             <div class="mb-3">
               <label class="form-label">Mikor volt feltöltve</label>
-              <input type="date" class="form-control">
+              <input type="date" class="form-control" name="refilldate" value="<?php echo $refilldate; ?>">
             </div>
             <div class="mb-3">
-              <label class="form-label">Ízvilág</label>
-              <input type="text" class="form-control">
+              <label class="form-label">Ízvilág <?php if (!$methodGet && $typeError) { echo '<span class="errorMessage">Nem lehet üres az ízvilág mező</span>'; } ?></label>
+              <input type="text" class="form-control" name="type" value="<?php echo $type; ?>">
             </div>
             <div class="mb-5">
-              <label class="form-label">Csipősségi szint</label>
-              <input type="number" class="form-control">
+              <label class="form-label">Csípősségi szint <?php if (!$methodGet && $hotlvlError) { echo '<span class="errorMessage">Nem lehet üres a csípősségi szint mező</span>'; } ?></label>
+              <input type="number" class="form-control" name="hotlvl" value="<?php echo $hotlvl; ?>">
             </div>
             <button type="submit" class="btn btn-primary">Hozzáadás</button>
           </form>
